7d6a9a00a27a216967429cd389e30a27bc7cd354
ghoti <ghoti2007@gmail.com>
2014-09-12 23:22:29 -0400

    lots of commits like this inc

--- plugins/ping.py
+++ plugins/ping.py
@@ -9,4 +9,4 @@ class PingBot(BotPlugin):
         #hardcoding the shit out of this for now, plans include pulling ldap info from j4lp.com and roster list
         #from room to avoid clutter and such  gross
         yield "rina_kondur chainsaw_mcginny otsdarva_iv tactically_superior_avacado tujiko_noriko"
-        yield "bowervvick_wowbagger impeh_man fenrir_vice vadrin_hegirin alistair_croup"
+        yield "bowervvick_wowbagger impeh_man fenrir_vice vadrin_hegirin alistair_croup inspector_gair"
