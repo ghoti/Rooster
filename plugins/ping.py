c0cc751d6c4a6a390c4321ddad44a4decdb89ece
ghoti <ghoti2007@gmail.com>
2014-09-15 17:23:00 -0400

    ping update

--- plugins/ping.py
+++ plugins/ping.py
@@ -1,5 +1,5 @@
 from errbot import BotPlugin, botcmd
-#!from errbot.builtins.webserver import webhook
+from errbot.builtins.webserver import webhook
 
 
 class Ping(BotPlugin):
@@ -8,37 +8,54 @@ class Ping(BotPlugin):
     min_err_version = '1.6.0'  # Optional, but recommended
     #max_err_version = '3.0.0'  # Optional, but recommended
 
-    groups = {}
-
     @botcmd(split_args_with="||")
     def ping_set(self, mess, args):
-
-        group_str = args[0]
+        """Create/Update a group."""
+        
+        group_str = str(args[0])
         group_tuple = group_str.split(" ", 1)
         
         if len(group_tuple) > 1:
             group, text = group_tuple
-            self.groups[group] = text
-            return "Updated group."
+            group = group.lower()
+            
+            old_value = self[group]
+            self[group] = text
+            if old_value:
+                return "Updated group '%s', previous value was: %s" % (group, old_value,)
+            else:
+                return "Created group '%s'." % (group,)
         else:
             group = group_tuple[0]
-            if group in self.groups:
-                del self.groups[group]
+            group = group.lower()
+            if group in self.keys():
+                del self[group]
                 return "Deleted group %s" % group
-        
-        
-        
         
     @botcmd(split_args_with=None)
     def ping(self, mess, args):
+        """Ping a specified group"""
         
-        if not args:
-            return "No group to ping."
-        group = args[0]
+        group = str(args[0])
+        group = group.lower()
         
-        group_text = self.groups.get(group, None)
+        group_text = self[group]
         
         if group_text is not None:
             return group_text
         else:
-            return "No such group"
+            return "No such group, valid groups are: %s" % (", ".join(sorted(self.keys())),)
+            
+    @botcmd(split_args_with=None)
+    def ping_groups(self, mess, args):
+        """Show the groups that can be pinged"""
+        
+        groups = self.keys()
+        
+        return ", ".join(sorted(groups))
+
+    def __getitem__(self, key):
+        try:
+            return super(Ping, self).__getitem__(key)
+        except KeyError:
+            return None
